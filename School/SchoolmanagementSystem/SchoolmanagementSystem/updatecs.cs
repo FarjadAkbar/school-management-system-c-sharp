73e0ee7578b693eaced2e4b1ab727d15010e6cec
FarjadAkbar <57658131+FarjadAkbar@users.noreply.github.com>
2019-11-12 12:46:35 +0500

    intial

---
+++ School/SchoolmanagementSystem/SchoolmanagementSystem/updatecs.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolmanagementSystem
+{
+    class updatecs
+    {
+
+        private string connstring = ConfigurationManager.ConnectionStrings["TESTDB"].ConnectionString;
+
+        public string update_std(student s)
+        {
+
+            string msg = "";
+
+            SqlConnection conn = new SqlConnection(connstring);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("updatestudents ", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("@std_name", SqlDbType.VarChar, 20).Value = s.s_name;
+
+                cmd.Parameters.Add("@std_fname", SqlDbType.VarChar, 20).Value = s.s_fname;
+                cmd.Parameters.Add("@gender", SqlDbType.VarChar, 20).Value = s.s_gender;
+                cmd.Parameters.Add("@std_address", SqlDbType.VarChar, 50).Value = s.s_address;
+                cmd.Parameters.Add("@std_admission_date", SqlDbType.VarChar, 20).Value = s.s_date;
+                cmd.Parameters.Add("@std_id_fk_id", SqlDbType.VarChar, 20).Value = s.s_fk;
+
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+
+
+                msg = "Data successfully inserted";
+
+
+            }
+            catch (Exception)
+            {
+                msg = "Data is not successfully inserted";
+
+
+
+            }
+
+
+            finally
+            {
+
+                conn.Close();
+
+
+            }
+            return msg;
+
+        }
+
+
+
+
+    }
+}
